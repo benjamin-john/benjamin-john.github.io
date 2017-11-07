9c7c1a600fdd92cf79e519552e8719f8f42779c7
benjamin-john <benjohnmn@outlook.com>
2017-11-07 13:19:38 -0600

    update

---
+++ action_page.php
@@ -0,0 +1,14 @@
+<?PHP
+$email = $_POST["emailaddress"];
+$fname = $_POST["fname"];
+$lname = $_POST["lname"];
+$message = $_POST["message"];
+$to = "benjamin-john@bethel.edu";
+$subject = "Contact Form Submission";
+$headers = "From: $email\n";
+$message = "A visitor to your site has submitted the contact form:\n
+Email Address: $email\n
+Name: $fname $lname\n
+Message: $message";
+mail($to,$subject,$message,$headers);
+?>
