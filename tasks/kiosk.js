3985a215645a0a50e5864d541ed59a65f0dd187f
benjamin-john <benjohnmn@outlook.com>
2018-09-21 13:37:19 -0500

    update

---
+++ tasks/kiosk.js
@@ -0,0 +1,16 @@
+
+function bodyOnload(){
+
+}
+function redirect() {
+    window.open("https://benjamin-john.com/tasks/", "_self");
+}
+
+function validate() {
+
+}
+
+function submitForm(){
+  var myVar = setTimeout(redirect, 200);
+  return true;
+}
