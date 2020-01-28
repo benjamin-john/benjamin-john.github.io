98ada5e0150ef64948aedd2191330c65e012e6cc
benjamin-john <benjamin-john@bethel.edu>
2020-01-28 16:09:22 -0600

    update

--- newtab/newtab.js
+++ newtab/newtab.js
@@ -99,6 +99,42 @@ function copyTextToClipboard() {
   }
 }
 
+function copyTextToClipboard2(myVar) {
+  var textArea = document.createElement("textarea");
+
+  // Place in top-left corner of screen regardless of scroll position.
+  textArea.style.position = 'fixed';
+  textArea.style.top = 0;
+  textArea.style.left = 0;
+
+  // Ensure it has a small width and height. Setting to 1px / 1em
+  // doesn't work as this gives a negative w/h on some browsers.
+  textArea.style.width = '2em';
+  textArea.style.height = '2em';
+
+  // We don't need padding, reducing the size if it does flash render.
+  textArea.style.padding = 0;
+
+  // Clean up any borders.
+  textArea.style.border = 'none';
+  textArea.style.outline = 'none';
+  textArea.style.boxShadow = 'none';
+
+  // Avoid flash of white box if rendered for any reason.
+  textArea.style.background = 'transparent';
+
+  textArea.value = myVar;
+  document.body.appendChild(textArea);
+  textArea.select();
+  var successful = document.execCommand('copy');
+  document.body.removeChild(textArea);
+  if(successful) {
+    alert("successfully Copied Calendar Shareable URL to Clipboard");
+  } else {
+    alert("Copy was unsuccessful");
+  }
+}
+
 function focusToSearch(){
   document.getElementById('searchBox').focus();
 }
