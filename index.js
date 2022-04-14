fd24746b5de7ddf2a55563f2c34b42c4ff28387a
benjamin-john <benjamin-john@bethel.edu>
2022-04-14 15:36:27 -0500

    update

--- index.js
+++ index.js
@@ -28,3 +28,17 @@ function contactButtonClick(){
     targetDiv.style.display = "block";
   }
 }
+
+function sendemail() {
+  Email.send({
+    Host : "smtp.yourisp.com",
+    Username : "username",
+    Password : "password",
+    To : 'them@website.com',
+    From : "you@isp.com",
+    Subject : "This is the subject",
+    Body : "And this is the body"
+}).then(
+  message => alert(message)
+);
+}
